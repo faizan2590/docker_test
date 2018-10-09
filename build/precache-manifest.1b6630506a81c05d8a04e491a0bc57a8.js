4372e363c98b274531012e54772b00228e0b18f1
faizan2590 <faizan2590@gmail.com>
2018-10-09 23:08:38 +0200

    initial commit

---
+++ build/precache-manifest.1b6630506a81c05d8a04e491a0bc57a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f910b1561c57e0c8220c",
+    "url": "/static/js/main.f910b156.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "f910b1561c57e0c8220c",
+    "url": "/static/css/main.beefe004.chunk.css"
+  },
+  {
+    "revision": "53de3ddee39464c4e32a29f7c0add205",
+    "url": "/index.html"
+  }
+];
